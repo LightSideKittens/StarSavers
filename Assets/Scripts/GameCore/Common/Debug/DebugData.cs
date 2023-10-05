41a7846004a54a5a49045819e9658c89a4c50772
Malvis Light <malvislight@gmail.com>
2023-10-05 08:18:33 +0400

    Added Debug Options❤️

--- Assets/Scripts/GameCore/Common/Debug/DebugData.cs
+++ Assets/Scripts/GameCore/Common/Debug/DebugData.cs
@@ -2,12 +2,11 @@
 
 namespace BeatRoyale
 {
-    public class DebugData : BaseDebugData<DebugData>
+    public partial class DebugData : BaseDebugData<DebugData>
     {
         public static DebugData Config => BaseDebugData<DebugData>.Config;
         public bool needShowRadius;
         public bool serverEnabled;
-        public bool infinityMana;
 
         public static void Save() => BaseDebugData<DebugData>.Save();
     }
---
+++ Assets/Scripts/GameCore/Common/Debug/DebugData_SROptions.cs
@@ -0,0 +1,33 @@
+﻿#if DEBUG
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.Scripting;
+
+namespace BeatRoyale
+{
+    public partial class DebugData
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+        public static void OnStartup()
+        {
+            SRDebug.Instance.AddOptionContainer(Config);
+        }
+        
+        [Category("Debug Data")]
+        [Preserve]
+        public bool NeedShowRadius
+        {
+            get => needShowRadius;
+            set => needShowRadius = value;
+        }
+    
+        [Category("Debug Data")]
+        [Preserve]
+        public bool ServerEnabled
+        {
+            get => serverEnabled;
+            set => serverEnabled = value;
+        }
+    }
+}
+#endif
