edb1e0768734e7f6a7fcd9141d7bb837f5677dd5
Malvis Light <malvislight@gmail.com>
2023-10-06 16:11:26 +0400

    Added UI blink❤️

---
+++ Assets/Scripts/GameCore/UIShape.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace LGCore.UIModule
+{
+    [RequireComponent(typeof(CanvasRenderer), typeof(RectTransform), typeof(MeshFilter)),  DisallowMultipleComponent]
+    public class UIShape : MaskableGraphic
+    {
+        public MeshFilter Shape { get; set; }
+        private Mesh mesh;
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            Shape = GetComponent<MeshFilter>();
+            mesh = new Mesh();
+        }
+
+        public override void Rebuild(CanvasUpdate update)
+        {
+            base.Rebuild(update);
+            if (update == CanvasUpdate.PreRender)
+            {
+                mesh = Shape.sharedMesh;
+                canvasRenderer.SetMesh(mesh);
+                var texture = material.mainTexture;
+                canvasRenderer.SetColor(color);
+
+                if (texture == null)
+                {
+                    texture = Texture2D.whiteTexture;
+                }
+                
+                canvasRenderer.SetTexture(texture);
+            }
+        }
+    }
+}
