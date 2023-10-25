80bb86f9560a04f1e9b4c9193cb679a0683878a0
Malvis Light <malvislight@gmail.com>
2023-10-25 10:58:06 +0400

    Implemented Tab System❤️

--- Assets/Scripts/GameCore/Launcher/Windows/BurgerPanel.cs
+++ Assets/Scripts/GameCore/Launcher/Windows/BurgerPanel.cs
@@ -1,11 +1,44 @@
 ﻿using LSCore;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace BeatHeroes.Windows
 {
     public class BurgerPanel : BaseWindow<BurgerPanel>
     {
+        [SerializeField] private RectTransform back;
+        [SerializeField] private Tab.Data buttonsTab;
+        [SerializeField] private Tab.Data[] tabs;
         
+        private Tab.Controller tabController;
+
+        protected override void Init()
+        {
+            base.Init();
+            tabController = new Tab.Controller(back);
+            tabController.OnOpen(OnTabOpen);
+            tabController.Add(buttonsTab);
+
+            for (int i = 0; i < tabs.Length; i++)
+            {
+                tabController.Register(tabs[i]);
+            }
+        }
+
+        private void OnTabOpen(Tab tab)
+        {
+            if (tab == buttonsTab.tabPrefab)
+            {
+                BackButton.OnClick(Hide);
+            }
+            else
+            {
+                BackButton.OnClick(OpenButtonsTab);
+            }
+        }
+
+        private void OpenButtonsTab()
+        {
+            tabController.Open(buttonsTab);
+        }
     }
 }
