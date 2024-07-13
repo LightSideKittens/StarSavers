bd0e5f4b958392cae542f886b5e7741a9a46e7c2
Malvis Light <cat@lightside.games>
2024-07-13 22:49:00 +0400

    First localization implementation in LSText❤️

---
+++ Assets/Scripts/Common/Data/Settings.cs
@@ -0,0 +1,25 @@
+﻿using LSCore.ConfigModule;
+using Newtonsoft.Json;
+using UnityEngine.Localization.Settings;
+
+namespace Common.Data
+{
+    public class Settings : BaseSingleConfig<Settings>
+    {
+        [JsonProperty] private string languageCode;
+        
+        public string LanguageCode
+        {
+            get => languageCode;
+            set
+            {
+                if (value != languageCode)
+                {
+                    var locale = LocalizationSettings.AvailableLocales.GetLocale(value);
+                    LocalizationSettings.SelectedLocale = locale;
+                    languageCode = value;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Launcher/Welcomer.cs
@@ -0,0 +1,13 @@
+﻿using LSCore;
+
+namespace MultiWars
+{
+    public class Welcomer : ServiceManager<Welcomer>
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+            
+        }
+    }
+}
