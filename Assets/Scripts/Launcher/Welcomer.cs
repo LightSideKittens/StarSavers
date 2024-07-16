7750929ed7ff0441af6bb3f30cebbece1327216a
Malvis Light <cat@lightside.games>
2024-07-16 21:08:20 +0400

    Implemented Remove Asset Loading❤️

--- Assets/Scripts/Launcher/Welcomer.cs
+++ Assets/Scripts/Launcher/Welcomer.cs
@@ -1,13 +1,35 @@
-﻿using LSCore;
+﻿using DG.Tweening;
+using LSCore;
+using LSCore.AnimationsModule.Animations;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
 
 namespace MultiWars
 {
     public class Welcomer : ServiceManager<Welcomer>
     {
+        [SerializeField] private SliderAnim sliderAnim;
+        [SerializeField] private SliderAnim completedSliderAnim;
+        private Tween tween;
+        
         protected override void Awake()
         {
             base.Awake();
+            tween = sliderAnim.Animate();
+            Addressables.LoadSceneAsync("Launcher").OnSuccess(OnSuccess).OnError(OnError);
             
+            return;
+
+            void OnSuccess()
+            {
+                tween.Kill();
+                completedSliderAnim.Animate();
+            }
+
+            void OnError()
+            {
+                tween.Kill();
+            }
         }
     }
 }
