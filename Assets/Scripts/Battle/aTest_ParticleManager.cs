5a78890084a8d42789efa2482142d0d03fc41ec6
Ka6aH4eJla <kirill.kobyscha@lightgamesinc.com>
2024-08-20 02:40:00 +1000

    KitsuneStage1 attack animations🐺

--- Assets/Scripts/Battle/aTest_ParticleManager.cs
+++ Assets/Scripts/Battle/aTest_ParticleManager.cs
@@ -1,10 +1,18 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class aTest_ParticleManager : MonoBehaviour
 {
+    
     public ParticleSystem hitEffect;
     public ParticleSystem smokeEffect;
     public ParticleSystem[] Claws;
+    public ParticleSystem swordWave;
+    public ParticleSystem sphereEffect;
+    public ParticleSystem enchantEffect;
+    
+    public ParticleSystem[] EnchantPortalKunai;
+
     
     private int currentIndex = 0;
     
@@ -22,4 +30,14 @@ public class aTest_ParticleManager : MonoBehaviour
     {
         currentIndex = 0;
     }
+    public void PlaySphereEffect() => sphereEffect.Play();
+
+    public void PlaySwordWaveEffect() => swordWave.Play();
+    public void PlayPortalEnchantEffect() => enchantEffect.Play();
+
+    public void PlayEnchantPortalsEffect()
+    {
+        EnchantPortalKunai[Random.Range(0, EnchantPortalKunai.Length)].Play();
+    }
+
 }
