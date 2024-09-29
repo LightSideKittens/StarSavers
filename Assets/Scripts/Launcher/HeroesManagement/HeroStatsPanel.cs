5d8e9efcf16f80e70ce5b502575d6195a53fd6d9
Malvis Light <cat@lightside.games>
2024-09-29 23:09:17 +0400

    Improved Spend- and Earn- FundActions❤️

--- Assets/Scripts/Launcher/HeroesManagement/HeroStatsPanel.cs
+++ Assets/Scripts/Launcher/HeroesManagement/HeroStatsPanel.cs
@@ -1,4 +1,6 @@
 ﻿using LSCore;
+using LSCore.BattleModule;
+using LSCore.LevelSystem;
 using UnityEngine;
 
 namespace Launcher.HeroesManagement
@@ -6,7 +8,14 @@ namespace Launcher.HeroesManagement
     public class HeroStatsPanel : MonoBehaviour
     {
         public HeroId heroId;
+        public LevelsManager heroesLevelsManager;
         public LSButton upgradeButton;
-        
+        public LSButton[] upgradeButtons;
+        public LSText levelText;
+
+        private void Awake()
+        {
+            heroesLevelsManager.GetCurrentLevel<Unit>(heroId.id);
+        }
     }
 }
